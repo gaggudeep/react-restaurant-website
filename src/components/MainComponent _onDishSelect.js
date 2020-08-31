613618e909d54666e4a7bc245ee8ed801a1fc370
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2020-09-01 00:48:06 +0530

    Header & Footer

--- src/components/MainComponent _onDishSelect.js
+++ src/components/MainComponent _onDishSelect.js
@@ -14,25 +14,20 @@ class Main extends Component{
 
         this.state = {
             dishes : DISHES,
+            selectedDish: null
         };
     }
 
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId});
+    }
+
     render () {
-
-        const HomePage = () => {
-            return (
-                <Home />
-            );
-        }
-
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-                    <Redirect to="/home" />
-                </Switch>
+                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                <Dishdetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
                 <Footer />
             </div>
         );
