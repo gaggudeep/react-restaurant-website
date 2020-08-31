ed025d4b0f0296716415cb616efff198ef3bf3b6
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2020-09-01 01:26:54 +0530

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,6 +4,20 @@ import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -13,7 +27,7 @@ class Header extends Component {
                         <NavbarBrand className="mr-auto" href="/">
                             <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
                         </NavbarBrand>
-                        <Collapse navbar>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
@@ -26,7 +40,7 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/enu">
+                                    <NavLink className="nav-link" to="/menu">
                                         <span className="fa fa-list fa-lg"> Menu</span>
                                     </NavLink>
                                 </NavItem>
